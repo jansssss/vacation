eec77ba06d0ca16a26ac699717726910bb3d22bf
jansssss <53560050+jansssss@users.noreply.github.com>
2025-06-12 21:04:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import AnnualLeaveCalculator from "../pages/AnnualLeaveCalculator";
-import RetirementCalculator from "../pages/RetirementCalculator";
+import Home from "./pages/Home";
+import AnnualLeaveCalculator from "./pages/AnnualLeaveCalculator";
+import RetirementCalculator from "./pages/RetirementCalculator";
 import Header from "./components/Header";
 
 function App() {
